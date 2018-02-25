ec78f2b343d1ec9d7fdf4878555eb93b862e1f19
xwjie <121509092@qq.com>
2018-02-25 23:56:36 +0800

    修改代码

--- xiaorpc/src/main/java/cn/xiaowenjie/xiaorpc/client/XiaoProxyFactory.java
+++ xiaorpc/src/main/java/cn/xiaowenjie/xiaorpc/client/XiaoProxyFactory.java
@@ -8,6 +8,9 @@ import java.util.Hashtable;
 
 import javax.naming.Context;
 import javax.naming.Name;
+import javax.naming.NamingException;
+import javax.naming.RefAddr;
+import javax.naming.Reference;
 import javax.naming.spi.ObjectFactory;
 
 import org.apache.http.client.HttpClient;
@@ -25,9 +28,41 @@ public class XiaoProxyFactory implements ObjectFactory {
 		return Proxy.newProxyInstance(loader, new Class<?>[] { service }, handler);
 	}
 
+	/**
+	 * JNDI object factory so the proxy can be used as a resource.
+	 */
 	public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment)
 			throws Exception {
-		return null;
+		Reference ref = (Reference) obj;
+
+	    String api = null;
+	    String url = null;
+	    
+	    for (int i = 0; i < ref.size(); i++) {
+	      RefAddr addr = ref.get(i);
+
+	      String type = addr.getType();
+	      String value = (String) addr.getContent();
+
+	      if (type.equals("type"))
+	        api = value;
+	      else if (type.equals("url"))
+	        url = value;
+	      //else if (type.equals("user"))
+	        //setUser(value);
+	      //else if (type.equals("password"))
+	        //setPassword(value);
+	    }
+
+	    if (url == null)
+	      throw new NamingException("`url' must be configured for HessianProxyFactory.");
+	    // XXX: could use meta protocol to grab this
+	    if (api == null)
+	      throw new NamingException("`type' must be configured for HessianProxyFactory.");
+
+	    Class apiClass = Class.forName(api, false, _loader);
+
+	    return create(apiClass, url);
 	}
 
 }
